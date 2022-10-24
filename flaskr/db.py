9ab22bc96bce93fa76db763f1b449978144a1e64
jmmoran4 <jmmoran@buffalo.edu>
2022-10-23 20:03:47 -0400

    test commit

---
+++ flaskr/db.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+# Initialize mongo db with app
+app = Flask(__name__)
+
+mongodb_client = PyMongo(app, uri='mongodb://localhost:27017/todo_db')
+db = mongodb_client.db
+
