4d275cf9476a8213db1916b1b4ac8bf077aa5cbc
jmmoran4 <jmmoran@buffalo.edu>
2022-11-02 19:46:14 -0400

    database up on local host

--- flaskr/db.py
+++ flaskr/db.py
@@ -7,3 +7,23 @@ app = Flask(__name__)
 mongodb_client = PyMongo(app, uri='mongodb://localhost:27017/todo_db')
 db = mongodb_client.db
 
+user_collection = db['users']
+leaderboard = db['leaderboard']
+
+# add user as {'username' : username, 'wins' : '0', 'loss' : '0'}
+def add_user(username):
+    pass
+
+# add a win or loss to the users stats
+def update_player_stats(username, stat_to_change, increment):
+    pass
+
+
+#change users score to {'username' : username, 'score' : new_score}... score will be a integer that ranks the player based on # games played and W/L ratio
+def update_leaderboard(username, new_score):
+    pass
+
+# retuns a dictionary of form {rank : [score, usernmae]}
+def get_leaderboard():
+    pass
+
